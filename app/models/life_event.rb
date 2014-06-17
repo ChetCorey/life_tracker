35cfe361d64bcd9eebd6e65acc3e1b25e769fc3a
Chet Corey <chetcorey@hotmail.com>
2014-06-17 16:10:14 -0400

    This life tracker

---
+++ app/models/life_event.rb
@@ -0,0 +1,6 @@
+# Life Events
+class LifeEvent < ActiveRecord::Base
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :description, presence: true, length: { maximum: 140 }
+end
---
+++ app/models/school.rb
@@ -0,0 +1,7 @@
+# school
+class School < ActiveRecord::Base
+  validates :school_name, presence: true
+  validates :beginning_year, presence: true, numericality: true
+  validates :ending_year, presence: true, numericality: true
+  # validates :beginning_year, numericality: {less_than_or_equal_to: :ending_year}
+end
